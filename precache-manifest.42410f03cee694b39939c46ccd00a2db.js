41e62dfcc9bcedf900a786dcf14cc32fd5ab48e7
Achyut Raj <achyutraj33@gmail.com>
2019-12-19 00:30:12 +0530

    Updates

---
+++ precache-manifest.42410f03cee694b39939c46ccd00a2db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbc9f42bf9c9f2a9537ec2fba174754",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "cbfc6c127538fcbd0523",
+    "url": "/tictactoe/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "7364c97a685afb1e3aaa",
+    "url": "/tictactoe/static/js/2.99f0d29d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tictactoe/static/js/2.99f0d29d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cbfc6c127538fcbd0523",
+    "url": "/tictactoe/static/js/main.18c0ee48.chunk.js"
+  },
+  {
+    "revision": "d5cef6b0326e4be2e1fe",
+    "url": "/tictactoe/static/js/runtime-main.d953a2bb.js"
+  }
+]);
